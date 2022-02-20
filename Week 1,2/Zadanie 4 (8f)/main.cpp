fb01b5492d874d4eca3757c57c8aa64aec7925d7
Konstantin Mutin <kostyamutin14@gmail.com>
2022-02-20 22:13:36 +0300

    Update main.cpp

--- Week 1,2/Zadanie 4 (8f)/main.cpp
+++ Week 1,2/Zadanie 4 (8f)/main.cpp
@@ -4,7 +4,7 @@ int main() {
 
 	float a = 0.1;
 	float b = 0.2;
-	int x = 1;
+	float x = 1;
 	float sn = sin(x + a);
 
 	std::cout << "Chislo=" << sqrt(x * x + b) - ((b * b * (sn * sn * sn)) / x) << std::endl;
